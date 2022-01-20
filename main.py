5702d56e4d4d9ef1ff9ab272098425fd47f51ee8
ESJvR <33872783+ESJvR@users.noreply.github.com>
2022-01-20 13:09:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,40 +20,13 @@ def createMap(x,y):
     MapUnlinked[0][0].content=-1
     return MapUnlinked[0][0]
 
-def BorderLine():
-    print("-------------------------------------------------------")
-
 def drawHelp():
     print("Here a list of the available commands:\n")
-    print("  !inv -> shows your inventory")
     print("  !move <direction> -> you move to the room in the given direction")
     print("    available directions are: up, down, left, right")
     print("  !pick -> collect all items in the room")
-    #print("  !stairs -> use the stairs to go to the next floor")
     print("  !fight -> use your current power to attack the local enemy")
-    print("  !stats -> shows your stats")
     print("  !use <itemName> -> use an item with given name")
-    print("    you can search for the name in the inventory")
-
-def drawInv(inventory):
-    if len(inventory)==0: print("Your inventory is empty, go explore some rooms, before you check your bag."); return
-    print("Here you can see all your current belongings:")
-    for i in inventory:
-        print("  "+i.type+": "+i.name+", with a strength of "+str(i.power))
-
-def drawStats(Player,Enemy):
-    print("let's take a look at you\n")
-    print("Your name is: "+Player.name)
-    print("You currently have "+ str(Player.Health)+ " HP")
-    print("You have "+ str(Player.Damage)+ " damage to deal")
-    print("You carry "+ str(Player.Gold)+ " Gold ounces with you")
-    if(not Enemy is None):
-        print("")
-        print("There is also an Enemy in your room!")
-        print("It is a "+Enemy.name)
-        print("It has "+str(Enemy.Health)+" HP")
-        print("It deals "+str(Enemy.Damage)+ " damage")
-    print("") 
 
 def PickOne(a):
     index=math.floor(random.random()*len(a))
@@ -61,13 +34,12 @@ def PickOne(a):
 
 def ItemNameGenerator(type,power):
     name=""
-    weaponNames=["Dagger","Bow","Sword","Pike","Staff"]
-    weaponEpicAttributes=["mystic","shiny","golden","sharp"]
-    weaponLegendaryAttributes=["deadly","legendary","magnificent","enhanced","reinforced"]
-    potionNames=["Health Potion","Potion of Regeneration","Life Potion","Bandages"]
-    potionAttributes=["big", "regenerating","tasty","red","pink"]
-    treasureNames=["Coins","Treasure Map","Monocle","Gem"]
-    treasureAttributes=["ancient","valuable","golden","famous"]
+    weaponNames=["Sword","Staff"]
+    weaponEpicAttributes=["mystic","shiny"]
+    weaponLegendaryAttributes=["deadly","legendary"]
+    potionNames=["Health Potion"]
+    potionAttributes=["red","pink"]
+
     if type=="weapon":
         if power>=50&power<75:
             name+=PickOne(weaponEpicAttributes)
@@ -81,11 +53,6 @@ def ItemNameGenerator(type,power):
             name+=PickOne(potionAttributes)
             name+=" "
         name+=PickOne(potionNames)
-    elif type=="treasure":
-        if power>=60:
-            name+=PickOne(treasureAttributes)
-            name+=" "
-        name+=PickOne(treasureNames)
     return name
 
 def loadRoom(Map, dir):
@@ -100,7 +67,6 @@ def loadRoom(Map, dir):
         newRoom=Map.left
     else:
         newRoom=Map.right
-    #print("DEBUG: content val is",newRoom.content)
     content=newRoom.content
     if(content==-1):
         print("The room is empty.\n")
@@ -112,29 +78,24 @@ def loadRoom(Map, dir):
         newRoom.content=-1
     if((content>=0.2)&(content<0.65)):
         print("You enter the room and on a podest next to you is a mysterious item, with some sunrays burning on it through a crack in the ceiling")
-        typ=PickOne(["potion","weapon","treasure"])
+        typ=PickOne(["potion","weapon"])
         power=math.ceil(random.random()*100)
         name=ItemNameGenerator(typ,power)
-        #print("DEBUG:",name,typ,power)
         placedItem=Item(name,power,typ)
         print("As you get closer you can clearly identify it as a "+typ)
+        print("On closer inspection its actually a "+name)
         print("\nYou can now pick it up or leave the room")
         print("yet an ancient inscribe on the podest says, that it will vanish as you leave the room")
         newRoom.content=-1
     if(content>=0.65):
         print("You enter the room and a dark mist fills the air..\n")
-        Enemy=Creature(PickOne(["bat","rat","goblin"]))
+        Enemy=Creature(PickOne(["goblin"]))
         print("You look around and as the fog starts to vanish you can clearly see the outline of a "+Enemy.name+"..")
         print("You'll need to fight it to get along.")
         newRoom.content=-1
-    #print("DEBUG: content val is",newRoom.content)
     return newRoom,placedItem,Enemy
 
-        
-    return newRoom,Item,Enemy
-
 def UseItem(name,Inv,Player):
-    #print("DEBUG: len of Inv",len(Inv),Inv)
     Item=None
     for i in range(len(Inv)):
         if Inv[i].name==name:
@@ -147,52 +108,28 @@ def UseItem(name,Inv,Player):
     else:
         if Item.type=="potion":
             Player.Health+=Item.power
+            print("You used the potion")
         if Item.type=="weapon":
             Player.Damage+=Item.power
-        if Item.type=="treasure":
-            print("You get out the "+Item.name)
-            print("it looks really good, you shouldn't lose it")
-            print("\nyou put it back into your bag")
-            Inv.append(Item)
-        
-
+            print("You took your weapon into your own hands")
+       
 class Creature:
-    Health=69
+    Health=100
     Damage=0
-    Gold=0
     name="wtf"
-    #Health, Damage, Gold,name
     def __init__(self,type):
-        #print("DEBUG: creating creature of type: "+ type)
         match(type):
             case "player":
                 self.Health=100
                 self.Damage=5
-                self.Gold=0
-                self.name="kekw"
-            case "bat":
-                self.Health=20
-                self.Damage=8
-                self.Gold=4
-                self.name="bat"
-            case "rat":
-                self.Health=16
-                self.Damage=8
-                self.Gold=4
-                self.name="rat"
+                self.name="Dio"
             case "goblin":
                 self.Health=30
                 self.Damage=3
-                self.Gold=20
                 self.name="goblin"
-    def ResetDamage(self):
-        if(self.Damage!=5):
-            print("Oh no, you weapon broke in combat, you'll have to equip a new one.")
-        self.Damage=5
+                
 class Item:
-    #Name, Powerlevel, Use
     def __init__(self,name,p,t):
-        #print("DEBUG: creating Item",name,p,t)
         self.name=name
         self.power=p
         self.type=t
@@ -201,21 +138,15 @@ Enemy=None
 PlacedItem=None
 Inventory=[]
 Map=createMap(8,8)
-print("Welcome to a cheap copy of Zorg, the great great grandfather of dwarf fortress, the only game you shall play!")
-Player.name=input("But before we begin, we need to settle on a name for our hero. What shall we call you?\n")
-BorderLine()
-print("Great",Player.name,"! Now you can start your journey.")
 print("\nYour adventure begins in a deep cavern. Stay alarmed at all times and be ready to fight for your life, only the true heroes will return rich and with glory.")
 print("Type !help for a list of commands\n")
 while(Player.Health>0):
     command=input("What are you doing now?\n")
     commandKey=command.split(" ")[0]
-    BorderLine()
+    print("-------------------------------------------------------")
     match(commandKey):
         case "!help":
             drawHelp()
-        case "!inv":
-            drawInv(Inventory)
         case "!move":
             if(Enemy is None):
                 print("You move into the next room\n")
@@ -231,7 +162,9 @@ while(Player.Health>0):
                 Inventory.append(PlacedItem)
                 PlacedItem=None
         case "!fight":
-            print("You engage in combat!")
+            if Enemy is None:
+                print("Are you already hallucinating? There is noone there !")
+            else: print("You menacingly approach the enemy ! Dio's Theme starts playing...")
             while((Enemy is not None)&(Player.Health>0)):
                 print("")
                 Enemy.Health-=Player.Damage
@@ -239,24 +172,8 @@ while(Player.Health>0):
                 Player.Health-=Enemy.Damage
                 print("You got struck for "+str(Enemy.Damage)+" damage and are now on "+str(Player.Health)+" HP.")
                 if(Enemy.Health<=0):
-                    Player.Gold+=Enemy.Gold
                     Enemy=None
-                    Player.ResetDamage()
-            if Player.Health>0:
-                print("You beat the opponent, congrats!\n")
-        case "!stats":
-            drawStats(Player,Enemy)
+                    print("You destroyed the opponent, congrats! Dio's Theme stops playing... \n")
         case "!use":
             UseItem(command[5:],Inventory,Player)
-        case x:
-            print("You shout \""+command+"\" into the cavern...\n")
-            print("Nothing seems to happen..")
-            print("Maybe the lonelyness starts to drive you insane,")
-            print("Type !help for help")
 print("Unfortunately your HP dropped to 0, this means game over")
-print("But it had a purpose!")
-print("You had "+Player.Gold+" Gold with you")
-totalWealth=sum(map(lambda x: x.power,list(filter(lambda x: x.type=="treasure",Inventory))))
-print("Plus, your treasures had a total wealth of "+totalWealth)
-print("That makes a total of "+str(Player.Gold+totalWealth)+" in gold coins, that you earned")
-print("It was a good game!")
